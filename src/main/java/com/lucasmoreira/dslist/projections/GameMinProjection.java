556ac35fbcb06bb1eaf460124124b387d7a652ce
Lucas Moreira <lucasmoreiralana04@gmail.com>
2025-05-21 11:43:54 -0300

    Homolog ok.

--- src/main/java/com/lucasmoreira/dslist/projections/GameMinProjection.java
+++ src/main/java/com/lucasmoreira/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.lucasmoreira.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
